c4c58c3d12c85cb01accbb0064c440531e5e56b5
climb719 <mary.oldweiler@gmail.com>
2021-09-01 12:53:18 -0400

    commit tree db067b9222ba4df357b955e0119a09ef9a3f5a2d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(name => name.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+    let n = string[0].toLocaleLowerCase()
+    return drivers.filter(name => n === name[0].toLocaleLowerCase() )
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(n => n.name === string )
+}
